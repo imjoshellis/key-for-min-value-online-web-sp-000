56efeec706f8b33c8a4966ea95c3dde69bb164b1
Josh Ellis <joshe523@gmail.com>
2020-03-03 05:22:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.size == 0
-  lowest = name_hash.first
+  lowest = nil
   name_hash.each { |name, n| n < lowest ? lowest = n : n }
   return lowest
 end
